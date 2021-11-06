f6751142824d9abfcdf4c5517507fda4c2ce3a02
mikoto2000 <mikoto2000@gmail.com>
2021-11-07 00:27:32 +0900

    ブックマークリスト表示機能追加。

--- src/Main.elm
+++ src/Main.elm
@@ -1,16 +1,55 @@
 module Main exposing (main, update, view)
 
 import Browser
-import Html exposing (Html, button, div, p, text)
+import Html exposing (Html, a, button, div, li, p, text, ul)
+import Html.Attributes exposing (href, id)
+import List
+import String
 
 
 main =
     Browser.sandbox { init = 0, update = update, view = view }
 
 
-update msg model =
+
+-- 型定義
+
+
+type alias Bookmark =
+    { title : String
+    , url : String
+    }
+
+
+
+-- ブックマークのリスト
+
+
+bookmarks =
+    [ { title = "Google"
+      , url = "https://www.google.com"
+      }
+    , { title = "TweetDeck"
+      , url = "https://tweetdeck.twitter.com/"
+      }
+    ]
+
+
+update _ model =
     model
 
 
 view model =
-    div [] [ p [] [ text "Hello, World!" ] ]
+    div []
+        [ div []
+            [ div [ id "bookmarks" ]
+                [ ul []
+                    (List.map to_anchor bookmarks)
+                ]
+            ]
+        ]
+
+
+to_anchor bookmark =
+    li []
+        [ a [ href bookmark.url ] [ text bookmark.title ] ]
